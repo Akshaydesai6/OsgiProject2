729f649e8b277e96646f8a6d73cac560fddb99ff
akshaydesai6 <akshay.desai@promation.com>
2023-07-27 14:54:34 +0530

    first commit

---
+++ src/main/java/osgi/Argumentbased.java
@@ -0,0 +1,38 @@
+package osgi;
+
+import java.util.Scanner;
+
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
+
+@Command(scope = "CustomCommand", name = "display1", description = "This is the  custom karaf command example ")
+@Service
+public class Argumentbased implements Action  {
+Scanner scanner=new Scanner(System.in);
+
+	@Argument(index = 0, name = "action", description = "action dkaraf@root()> CustomCommand:display efines what action to be performed", required = true, multiValued = false)
+	String name=null;
+	public Object execute() throws Exception {
+		
+		if(name.equals("add")) {
+			int a=scanner.nextInt();
+			int b=scanner.nextInt();
+			System.out.println(a+b);
+			return a+b;
+		}
+		
+		else {
+			String a=scanner.next();
+			String b=scanner.next();
+			System.out.println(a+b);
+			return a+b;
+
+		}
+		
+		
+	}
+}
